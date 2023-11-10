eef1ea90a14dd95b0a0465d9311c34635c6b7cf3
Сергей Cуворов <Se7g0oo@mail.ru>
2023-11-11 00:21:41 +0300

    task 3 complite

--- Тема - Структуры хранения данных - list, tuple, dict, set/3.py
+++ Тема - Структуры хранения данных - list, tuple, dict, set/3.py
@@ -1,1 +1,33 @@
-s1afwef
+london_co = {
+    "r1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.1",
+    },
+    "r2": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.2",
+    },
+    "sw1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "3850",
+        "ios": "3.6.XE",
+        "ip": "10.255.0.101",
+        "vlans": "10,20,30",
+        "routing": True,
+    },
+}
+a= input("enter device: ")
+c=tuple(london_co.get(a).keys())
+#print(c) #проверяем тип для использования метода
+print("enter parametrs",c,":")
+b=input()
+# print(type(c))
+print(london_co.get(a).get(b))
+
