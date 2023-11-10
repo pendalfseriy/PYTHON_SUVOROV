ab980e659a32636ff007e8c117b4fc1a02e700a0
Сергей Cуворов <Se7g0oo@mail.ru>
2023-11-10 23:31:53 +0300

    task 2 complite

---
+++ Тема - Структуры хранения данных - list, tuple, dict, set/2.py
@@ -0,0 +1,31 @@
+london_co = {
+    "r1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.1",
+    },
+    "r2": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.2",
+    },
+    "sw1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "3850",
+        "ios": "3.6.XE",
+        "ip": "10.255.0.101",
+        "vlans": "10,20,30",
+        "routing": True,
+    },
+}
+a= input("enter device: ")
+b=input("enter parametr: ")
+c=(london_co.get(a))
+# print(c) проверяем тип для использования метода
+# print(type(c))
+print(c.get(b))
