b75f4f345329bee6fdff99a5b01bbb3211595fbb
Сергей Cуворов <Se7g0oo@mail.ru>
2024-03-09 15:13:40 +0300

    done Task3

--- DZ/Циклы/Task3.py
+++ DZ/Циклы/Task3.py
@@ -28,17 +28,8 @@ switchport trunk encapsulation dot1q
 switchport mode trunk
 switchport trunk allowed vlan remove 17
 Генератор для access портов представлен ниже
-access_template = [
-"switchport mode access",
-"switchport access vlan",
-"spanning-tree portfast",
-"spanning-tree bpduguard enable",
-]
-trunk_template = [
-"switchport trunk encapsulation dot1q",
-"switchport mode trunk",
-"switchport trunk allowed vlan",
-]
+access_template = ["switchport mode access","switchport access vlan","spanning-tree portfast","spanning-tree bpduguard enable",]
+trunk_template = ["switchport trunk encapsulation dot1q","switchport mode trunk","switchport trunk allowed vlan",]
 access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
 trunk = {"0/1": ["add", "10", "20"], "0/2": ["only", "11", "30"], "0/4": ["del", "17"]}
 for intf, vlan in access.items():
@@ -47,5 +38,38 @@ for command in access_template:
 if command.endswith("access vlan"):
 print(f" {command} {vlan}")
 else:
-print(f" {command}")
+print(f" {command}") 
 """
+
+# access_template = ["switchport mode access","switchport access vlan","spanning-tree portfast","spanning-tree bpduguard enable",]
+# trunk_template = ["switchport trunk encapsulation dot1q","switchport mode trunk","switchport trunk allowed vlan",]
+# access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
+# trunk = {"0/1": ["add", "10", "20"], "0/2": ["only", "11", "30"], "0/4": ["del", "17"]}
+
+  
+access_template = ["switchport mode access","switchport access vlan","spanning-tree portfast","spanning-tree bpduguard enable"]
+trunk_template = ["switchport trunk encapsulation dot1q","switchport mode trunk","switchport trunk allowed vlan"]
+access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
+trunk = {"0/1": ["add", "10", "20"], "0/2": ["only", "11", "30"], "0/4": ["del", "17"]}
+# for intf, vlan in access.items():
+#     print("interface FastEthernet " + intf)
+#     for command in access_template:
+#         if command.endswith("access vlan"):
+#             print(f"{command} {vlan}")
+#         else:
+#             print(f"{command}") 
+a=list(trunk.values())
+print(a[0][0])
+a=a[0][1:].copy()
+
+for intf, vlan in list(trunk.items()):            
+    print("interface FastEthernet " + intf)
+    if list(trunk.items())[intf][0]=="add":
+
+        for command in trunk_template:
+
+
+            if command.endswith("switchport trunk allowed vlan"):
+                print(f"{command} {trunk.items()[1:]}")
+            else:
+                print(f"{command}") 
